a569b2659399e5c8a68f0e2e94bc365e3bbf8ef3
DESKTOP-DLOPF19\SSAFY <wnsdh8846@naver.com>
2025-10-15 11:12:25 +0900

    [SUGYO-16] feat: 파일 서비스 기능 구현
    파일 형식 감지 Tika 의존성 추가, 파일 타입 enum 추가, 파일 서비스 기능 구현, 파일 관련 ErrorCode 작성

---
+++ src/main/java/store/sugyo/common/service/FileStorageService.java
@@ -0,0 +1,92 @@
+package store.sugyo.common.service;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.tika.Tika;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import store.sugyo.common.domain.ImageFileExtension;
+import store.sugyo.common.exception.ApplicationException;
+import store.sugyo.common.repository.ObjectStorageRepository;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+import static store.sugyo.common.exception.CommonErrorCode.*;
+
+
+@Service
+@RequiredArgsConstructor
+public class FileStorageService {
+
+    private final ObjectStorageRepository objectStorageRepository;
+    private final Tika tika;
+
+    public String uploadFile(MultipartFile file, String subPath) throws IOException {
+        if (file == null || file.isEmpty()) {
+            return null;
+        }
+
+        ImageFileExtension fileExtension = getFileExtension(file);
+
+        String uniqueFileName = UUID.randomUUID() + "." + fileExtension.name().toLowerCase();
+
+        try {
+            String uploadedFileKey = objectStorageRepository.upload(
+                    subPath,
+                    uniqueFileName,
+                    file.getInputStream()
+            );
+            return objectStorageRepository.getDownloadUrl(uploadedFileKey);
+        } catch (IOException e){
+            throw new ApplicationException(FILE_READ_FAILED);
+        } catch (Exception e){
+            throw new ApplicationException(FILE_UPLOAD_FAILED);
+        }
+    }
+
+    public void deleteFile(String fileUrl) {
+        try {
+            objectStorageRepository.delete(fileUrl);
+        } catch (Exception e){
+            throw new ApplicationException(FILE_DELETE_FAILED);
+        }
+    }
+
+    private ImageFileExtension getFileExtension(MultipartFile file) throws IOException {
+        String originalFileName = file.getOriginalFilename();
+        if (originalFileName == null || originalFileName.isBlank()) {
+            throw new ApplicationException(INVALID_FILE_NAME);
+        }
+
+        String fileExtensionStr = StringUtils.getFilenameExtension(originalFileName);
+        if (fileExtensionStr == null) {
+            throw new ApplicationException(MISSING_FILE_EXTENSION);
+        }
+
+        ImageFileExtension fileExtension = getImageFileExtension(fileExtensionStr.toUpperCase());
+        validateMimeType(file, fileExtension);
+
+        return fileExtension;
+    }
+
+    private void validateMimeType(MultipartFile file, ImageFileExtension extension) throws IOException {
+        try (InputStream inputStream = file.getInputStream()) {
+            String detectedMimeType = tika.detect(inputStream);
+            if (detectedMimeType == null) {
+                throw new ApplicationException(UNREADABLE_FILE_MIME_TYPE);
+            }
+            if (!extension.getMimeType().equals(detectedMimeType.toLowerCase())) {
+                throw new ApplicationException(MIME_TYPE_MISMATCH);
+            }
+        } catch (IOException e) {
+            throw new ApplicationException(FILE_READ_FAILED);
+        }
+    }
+
+    private ImageFileExtension getImageFileExtension(String fileExtensionStr) {
+        return ImageFileExtension.fromExtension(fileExtensionStr)
+                .orElseThrow(() -> new ApplicationException(UNSUPPORTED_IMAGE_FILE_EXTENSION));
+    }
+}
